0b765aacaf4710694fd35f8f493e9712989c9cad
matteo-ms <108657821+matteo-ms@users.noreply.github.com>
2024-03-07 12:31:17 +0100

    Test Push Favicon

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="mt-24">
       <div className="my-8 md:order-1 md:mt-0">
         <p className="text-center text-xs leading-5 text-gray-500">
-          &copy; {currentYear} EWC, All rights reserved.
+          &copy; {currentYear} InfoCert, All rights reserved.
         </p>
       </div>
     </footer>
